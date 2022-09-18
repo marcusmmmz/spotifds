6df5d0f103800bebfa55787d27178e4cd9b6cac1
marcusmmmz <marcusizabel230@gmail.com>
2022-09-18 19:42:42 -0300

    fix vercel deploy

--- vite.config.ts
+++ vite.config.ts
@@ -8,16 +8,16 @@ const config: UserConfig = {
 		minify: false,
 		// disable @rollup/plugin-commonjs https://github.com/vitejs/vite/issues/9703#issuecomment-1216662109
 		// should be removable with vite 4 https://vitejs.dev/blog/announcing-vite3.html#esbuild-deps-optimization-at-build-time-experimental
-		commonjsOptions: {
-			include: [],
-		},
+		// commonjsOptions: {
+		// 	include: [],
+		// },
 	},
 	define: {
 		"process.env.NODE_DEBUG": "false",
 		global: "globalThis",
 	},
 	optimizeDeps: {
-		disabled: false,
+		// disabled: false,
 		esbuildOptions: {
 			target: "es2020",
 		},
