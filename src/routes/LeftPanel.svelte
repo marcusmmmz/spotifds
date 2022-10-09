f95a5a52ed4d370ba6932900a5ef54a985d8b4f8
Rotfuchs-von-Vulpes <derfuchs.pa@gmail.com>
2022-10-09 14:42:24 -0400

    change leftPanel style

--- src/routes/LeftPanel.svelte
+++ src/routes/LeftPanel.svelte
@@ -7,10 +7,10 @@
 
 <style>
 	.left {
+		background-color: var(--primary-background-color);
 		grid-area: panel;
-		width: 25%;
-		max-width: 25%;
 		padding: 25px 25px;
+		box-shadow: 0px 0px 5px black;
 	}
 
 	.left a {
@@ -18,8 +18,10 @@
 	}
 
 	.menuSelector {
+		width: 100%;
 		display: flex;
 		flex-direction: column;
+		margin-left: 10px;
 	}
 
 	.menuSelector a {
@@ -27,11 +29,22 @@
 		font-size: 22px;
 		color: #ffffff;
 		transition: 0.3s;
-		padding: 8px 8px;
+		padding-bottom: 10px;
 	}
 
 	.menuSelector a:hover {
 		transition: 0.3s;
-		background-color: rgba(255, 255, 255, 0.3);
+		padding-left: 10px;
+	}
+
+
+	.menuSelector a:active {
+		color: var(--yellow);
+		position: relative;
+		top: 2px;
+		transition: 0s;
+		text-shadow: 0 0 3px var(--yellow), 0 0 5px var(--yellow), 0 0 8px var(--yellow),
+			0 0 18px var(--orange), 0 0 30px var(--orange), 0 0 34px var(--orange),
+			0 0 40px var(--orange), 0 0 65px var(--orange);
 	}
 </style>
